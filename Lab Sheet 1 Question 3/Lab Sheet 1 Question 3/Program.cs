606507f57ea3e6ac1b9aec8e77960d3148ab0b0e
RavinduSohan <135201305+RavinduSohan@users.noreply.github.com>
2024-04-20 02:04:03 +0530

    Add files via upload

---
+++ Lab Sheet 1 Question 3/Lab Sheet 1 Question 3/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Lab_Sheet_1_Question_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a positive integer: ");
+            int n = int.Parse(Console.ReadLine());
+
+            if (n < 0)
+            {
+                Console.WriteLine("ERROR");
+            }
+            else
+            {
+                int sum = 0;
+                for (int i = 1; i <= n; i++)
+                {
+                    sum += i;
+                }
+                Console.WriteLine($"The sum of all numbers from 1 to {n} is: {sum}");
+            }
+        }
+    }
+}
