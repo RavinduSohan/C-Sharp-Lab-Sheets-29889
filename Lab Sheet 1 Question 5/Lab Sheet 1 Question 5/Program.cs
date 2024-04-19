50979727ea213395b7536df849de63d6f2d1e896
RavinduSohan <135201305+RavinduSohan@users.noreply.github.com>
2024-04-20 02:05:28 +0530

    Add files via upload

---
+++ Lab Sheet 1 Question 5/Lab Sheet 1 Question 5/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Lab_Sheet_1_Question_5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a number you wish to see the multiplication table for : ");
+            int num = int.Parse(Console.ReadLine());
+
+            Console.WriteLine($"Multiplication Table for {num}:");
+
+            for (int i = 1; i <= 10; i++)
+            {
+                int product = num * i;
+                Console.WriteLine($"{num} x {i} = {product}");
+            }
+        }
+    }
+}
