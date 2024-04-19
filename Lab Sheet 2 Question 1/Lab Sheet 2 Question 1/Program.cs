0efc6abe3ff6d8dc08f039b4b8a0812a4365b5ab
RavinduSohan <135201305+RavinduSohan@users.noreply.github.com>
2024-04-20 02:07:43 +0530

    Add files via upload

---
+++ Lab Sheet 2 Question 1/Lab Sheet 2 Question 1/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Lab_Sheet_2_Question_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Book book = new Book();
+
+            book.Title = "The Great Gatsby";
+            book.Author = "F. Scott Fitzgerald";
+
+            Console.WriteLine($"Book Title: {book.Title}");
+            Console.WriteLine($"Book Author: {book.Author}");
+        }
+    }
+
+    class Book
+    {
+        public string Title { get; set; }
+        public string Author { get; set; }
+    }
+}
