374e46c8df0c3e809f17757997b338f6e12820ee
RavinduSohan <135201305+RavinduSohan@users.noreply.github.com>
2024-04-20 02:03:17 +0530

    Add files via upload

---
+++ Lab Sheet 1 Question 2/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Lab_Sheet_1_Question_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter 10 numbers:");
+
+            for (int i = 1; i <= 10; i++)
+            {
+                Console.Write($"Number {i}: ");
+                int num = int.Parse(Console.ReadLine());
+
+                if (num % 2 == 0)
+                {
+                    Console.WriteLine($"{num} is even.");
+                }
+                else
+                {
+                    Console.WriteLine($"{num} is odd.");
+                }
+            }
+
+        }
+    }
+}
