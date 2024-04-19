8c54946f3f3a8d3610959d71271f7e211ef9c80a
RavinduSohan <135201305+RavinduSohan@users.noreply.github.com>
2024-04-20 02:02:39 +0530

    Add files via upload

---
+++ Lab Sheet 1 Question 1/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Question_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int length, width;
+
+            Console.WriteLine("Enter the length of the rectangle: ");
+            length = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter the width of the rectangle: ");
+            width = Convert.ToInt32(Console.ReadLine());
+
+            CalculateArea(length, width);
+
+            Console.ReadLine();
+        }
+
+       static void CalculateArea(int length, int width)
+        {
+            int area = length * width;
+            Console.WriteLine("The area of the rectangle is: " + area);
+        }
+    }
+}
